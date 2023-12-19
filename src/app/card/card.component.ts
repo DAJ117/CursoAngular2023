ed3fd9c7e291836beddfe83c27d53b17ab27e3e9
Diego Jara <diegotv_97@hotmail.com>
2023-12-18 22:51:41 -0500

    Version 1

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+
+  public image:string="https://img.freepik.com/fotos-premium/hermosos-paisajes-paisajes-naturales-hacen-que-personas-relajen-disfruten-fondo-pantalla_917506-214429.jpg";
+  public Titulo:string="Curso de Angular con Interpolación";
+  constructor(){}
+
+  ngOnInit(): void{}
+
+}
